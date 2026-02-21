f93df80ad58551b34e3a0abb17ae7b32463bc97e
petercai-wg <petercwg630@gmail.com>
2026-02-20 20:02:05 -0500

    20260220

--- django_docker/mywebsite/settings.py
+++ django_docker/mywebsite/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = os.getenv(
-    "DJANGO_CSRF_TRUSTED_ORIGINS", "https://127.0.0.1"
+    "DJANGO_CSRF_TRUSTED_ORIGINS", "http://127.0.0.1,https://127.0.0.1"
 ).split(",")
 
 
